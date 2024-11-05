46838ab25c209df522351fffe341916b6a5aa884
xJvstvs <justusttv@gmail.com>
2024-11-05 09:41:59 +0100

    test commit//

--- src/main/java/com/G/passwordvalidator/CustomPasswordValidator.java
+++ src/main/java/com/G/passwordvalidator/CustomPasswordValidator.java
@@ -7,7 +7,13 @@ package com.G.passwordvalidator;
 public class CustomPasswordValidator implements PasswordValidator {
     @Override
     public boolean isValid(String password) {
-        // Implementation logic here
+        // 8 Zeichen min
+        // min 1 Großbuchstaben
+        // min 1 Kleinbuchstaben
+        // min 1 Ziffer
+        // leer ""
+        // null
+        // Sponsoren
         return false;//to be done
         // TEST
 
